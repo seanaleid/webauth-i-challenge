ef53a8e9f29840f7aa8f147d380e3d486ec80c93
seanaleid <seanaleid@gmail.com>
2019-11-11 16:55:52 -0500

    Adds functional code for /api/auth/login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -26,9 +26,9 @@ router.post('/login', (req, res) => {
         .first()
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
-                res.status(200).json({ message: `Welcome ${user.username}!` });
+                res.status(200).json({ message: `Welcome ${user.username}! You are now logged in.` });
             } else {
-                res.status(401).json({ message: 'Invalid Credentials' });
+                res.status(401).json({ message: 'You shall not pass!' });
             }
         })
         .catch(err => {
