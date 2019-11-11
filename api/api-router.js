47daf97ab8aee3c4b63b27d01c7e167a78db5f2b
seanaleid <seanaleid@gmail.com>
2019-11-11 18:11:32 -0500

    Adds functional code for GET /api/restricted using global middleware

--- api/api-router.js
+++ api/api-router.js
@@ -2,11 +2,36 @@ const router = require('express').Router();
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const restrictedRouter = require('../restricted/restricted-router.js');
+
+const Users = require('../users/users-helpers.js');
 
 const bcrypt = require('bcryptjs');
 
+function gateKeeper(req, res, next) {
+    let { username, password} = req.headers;
+
+    if(username && password) {
+        Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                next();
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+    } else {
+        res.status(400).json({ message: 'You shall not pass!'});
+    }
+}
+
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
+router.use('/restricted', gateKeeper, restrictedRouter)
 
 router.get('/', (req, res) => {
     res.json({ api: "It's Alive"});
